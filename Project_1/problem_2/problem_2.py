b6f9ad959e5987c834160f6b5fb7ee86cc4f991b
LeopoldWalther <leopold.walther@gmail.com>
2021-05-19 18:36:30 +0200

    feat: solution to problem2

--- Project_1/problem_2/problem_2.py
+++ Project_1/problem_2/problem_2.py
@@ -1,30 +1,12 @@
-# For this problem, the goal is to write code for finding all files under a directory (and all directories beneath it) that end with ".c"
-
-
-## Locally save and call this file ex.py ##
-
-# Code to demonstrate the use of some of the OS modules in python
+# For this problem, the goal is to write code for finding all files under a directory
+# (and all directories beneath it) that end with ".c"
 
 import os
-
-# Let us print the files in the directory in which you are running this script
-print(os.listdir("."))
-
-# Let us check if this file is indeed a file!
-print(os.path.isfile("./problem_2.py"))
-
-# Does the file end with .py?
-print("./ex.py".endswith(".py"))
 
 
 def find_files(suffix, path):
     """
     Find all files beneath path with file name suffix.
-
-    Note that a path may contain further subdirectories
-    and those subdirectories may also contain further subdirectories.
-
-    There are no limit to the depth of the subdirectories can be.
 
     Args:
       suffix(str): suffix if the file name to be found
@@ -33,5 +15,35 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    if not os.path.isdir(path):  # Check if user
+        return 'Invalid path'
 
+    found_entities = os.listdir(os.path.join(path, "."))
+    paths = list()
+
+    for entity in found_entities:
+        if os.path.isdir(os.path.join(path, entity)):
+            paths += find_files(suffix, os.path.join(path, entity))
+
+        elif entity.endswith(suffix):
+            paths.append(os.path.join(path, entity))
+
+    return paths
+
+
+# Tests
+
+print(find_files('.c', './testdir/'))
+#  returns ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']
+
+print(find_files('.c', './testdir/subdir1/'))
+#  returns ['./testdir/subdir1/a.c']
+
+print(find_files('.c', '.'))
+#  returns ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']
+
+print(find_files('.h', '.'))
+#  returns ['./testdir/subdir3/subsubdir1/b.h', './testdir/subdir5/a.h', './testdir/t1.h', './testdir/subdir1/a.h']
+
+print(find_files('.h', ''))
+# returns error
