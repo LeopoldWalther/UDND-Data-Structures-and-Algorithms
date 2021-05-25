0ef10b7eff4e0cef725c8b151dc5da9ff81146c8
LeopoldWalther <leopold.walther@gmail.com>
2021-05-25 16:24:27 +0200

    feat: Implement minHeap as priority queue

--- Project_1/problem_3/problem_3.py
+++ Project_1/problem_3/problem_3.py
@@ -9,7 +9,73 @@ def huffman_decoding(data, tree):
     pass
 
 
-# heap
+class MinHeap(object):
+
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.size = 0
+        self.heap = [0] * (self.maxsize + 1)
+        self.ROOT = 1  # CONSTANT
+
+    def get_parent(self, position):
+        return position // 2
+
+    def get_left_child(self, position):
+        return 2 * position
+
+    def get_right_child(self, position):
+        return (2 * position) + 1
+
+    def is_leaf(self, position):
+        if (self.size // 2) <= position <= self.size:
+            return True
+        return False
+
+    def swap(self, first_position, second_position):
+        self.heap[second_position], self.heap[first_position] = self.heap[first_position], self.heap[second_position]
+
+    def insert(self, element):
+        if self.size >= self.maxsize:
+            return
+
+        self.size += 1
+        self.heap[self.size] = element
+
+        current_position = self.size
+
+        while self.heap[current_position] < self.heap[self.get_parent(current_position)]:
+            self.swap(current_position, self.get_parent(current_position))
+            current_position = self.get_parent(current_position)
+
+    def remove(self):
+        popped = self.heap[self.ROOT]
+        self.heap[self.ROOT] = self.heap[self.size]
+        self.size -= 1
+        self.heapify_node(self.ROOT)
+        return popped
+
+    def heapify_node(self, position):
+        if not self.is_leaf(position):
+            if self.heap[position] > self.heap[self.get_left_child(position)] or self.heap[position] > self.heap[self.get_right_child(position)]:
+                if self.heap[self.get_left_child(position)] < self.heap[self.get_right_child(position)]:
+                    self.swap(position, self.get_left_child(position))
+                    self.heapify_node(self.get_left_child(position))
+
+                else:
+                    self.swap(position, self.get_right_child(position))
+                    self.heapify_node(self.get_right_child(position))
+
+    def Print(self):
+        for i in range(1, (self.size//2)+1):
+            print(" PARENT : "+ str(self.heap[i])+" LEFT CHILD : "+
+                  str(self.heap[2 * i])+" RIGHT CHILD : "+
+                  str(self.heap[2 * i + 1]))
+
+    def build_heap(self):
+        for position in range(self.size//2, 0, -1):
+            self.heapify_node(position)
+
+
 class BinaryTreeNode(object):
 
     def __init__(self, character=None):
@@ -39,6 +105,7 @@ class BinaryTreeNode(object):
         else:
             True
 
+
 class BinaryTree(object):
 
     def __init__(self, character=None):
@@ -49,6 +116,40 @@ class BinaryTree(object):
 
 
 if __name__ == "__main__":
+
+    # test heap
+    minHeap = MinHeap(15)
+    values = [6, 2, 17, 120, 44, 129, 1, 22, 19]
+    for value in values:
+        minHeap.insert(value)
+    minHeap.build_heap()
+    minHeap.Print()
+    """
+    Expected output:  
+        PARENT : 1 LEFT CHILD : 6 RIGHT CHILD : 2
+        PARENT : 6 LEFT CHILD : 19 RIGHT CHILD : 44
+        PARENT : 2 LEFT CHILD : 129 RIGHT CHILD : 17
+        PARENT : 19 LEFT CHILD : 120 RIGHT CHILD : 22
+    """
+    print("Min value: " + str(minHeap.remove()))
+    # expected output: 1
+
+    minHeap.Print()
+    """
+    Expected output: 
+        PARENT : 2 LEFT CHILD : 6 RIGHT CHILD : 17
+        PARENT : 6 LEFT CHILD : 19 RIGHT CHILD : 44
+        PARENT : 17 LEFT CHILD : 129 RIGHT CHILD : 22
+        PARENT : 19 LEFT CHILD : 120 RIGHT CHILD : 22
+    """
+
+
+    print("New Min value: " + str(minHeap.remove()))
+    # expected output: 2
+
+    
+    """
+
     codes = {}
 
     a_great_sentence = "The bird is the word"
@@ -65,3 +166,5 @@ if __name__ == "__main__":
 
     print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
     print("The content of the encoded data is: {}\n".format(decoded_data))
+    
+    """
