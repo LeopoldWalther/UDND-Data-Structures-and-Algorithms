6d26812cf598ddb67920a68aed9717107e4b7dc5
LeopoldWalther <leopold.walther@gmail.com>
2021-06-01 18:10:03 +0200

    feat: add edge test case with one empty list

--- Project_1/problem_6/problem_6.py
+++ Project_1/problem_6/problem_6.py
@@ -198,3 +198,24 @@ print(union(linked_list_9, linked_list_10))
 # -1 -> -2 -> -2.1 -> -2.1 ->
 print(intersection(linked_list_9, linked_list_10))
 # -2.1 ->
+
+
+# Test case 6
+
+linked_list_9 = LinkedList()
+linked_list_10 = LinkedList()
+
+element_1 = [-1, -2, -2.1]
+element_2 = []
+
+for i in element_1:
+    linked_list_9.append(i)
+
+for i in element_2:
+    linked_list_10.append(i)
+
+print(union(linked_list_9, linked_list_10))
+# -1 -> -2 -> -2.1 -> -2.1 ->
+print(intersection(linked_list_9, linked_list_10))
+# no output
+
