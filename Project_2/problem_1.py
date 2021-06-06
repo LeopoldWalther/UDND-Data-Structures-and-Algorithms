bf25908d24923ea195e5cd9667d6d68b1174ee5b
LeopoldWalther <leopold.walther@gmail.com>
2021-06-06 11:39:02 +0200

    feat: solution for problem_1

--- Project_2/problem_1.py
+++ Project_2/problem_1.py
@@ -1,6 +1,6 @@
 # O(log(n))
 
-def sqrt(number):
+def sqrt(number: int) -> int:
     """
     Calculate the floored square root of a number
 
@@ -9,7 +9,35 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    pass
+    if number is None:
+        print('Input has to be of type integer.')
+        return None
+
+    elif number < 0:
+        return -1
+
+    elif number == 0 or number == 1:
+        return number
+
+    else:
+        start = 1
+        end = number
+
+        while start <= end:
+            mid = end - start // 2
+            squared_mid = mid ** 2
+
+            if squared_mid == number:
+                return mid
+
+            elif squared_mid < number:
+                start = mid + 1
+                guess = mid
+
+            else:
+                end = mid - 1
+
+        return guess
 
 
 print("Pass" if (3 == sqrt(9)) else "Fail")
