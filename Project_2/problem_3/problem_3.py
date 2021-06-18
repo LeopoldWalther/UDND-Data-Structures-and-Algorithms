0210faad3af2665682a91968b6373f7f7f2579da
LeopoldWalther <leopold.walther@gmail.com>
2021-06-18 18:30:19 +0200

    feat: implement mergesort

--- Project_2/problem_3/problem_3.py
+++ Project_2/problem_3/problem_3.py
@@ -6,7 +6,8 @@
 # for e.g. [1, 2, 3, 4, 5] the expected answer would be [531, 42]. Another expected answer can be [542, 31].
 # In scenarios such as these when there are more than one possible answers, return any one.
 
-
+# sort input list
+# go through input list and alternately add element to both output numbers
 
 def rearrange_digits(input_list):
     """
@@ -17,8 +18,50 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    pass
+    sorted_input_list = mergesort(input_list)
+    print(sorted_input_list)
 
+
+def mergesort(items):
+    """Mergesort by dividing array into single elements and sorting when merging bac together"""
+    # base case
+    if len(items) <= 1:
+        return items
+
+    # create sub-arrays
+    mid_index = len(items) // 2
+    left_sub_array = items[:mid_index]
+    right_sub_array = items[mid_index:]
+
+    # recursive calls
+    left_sub_array = mergesort(left_sub_array)
+    right_sub_array = mergesort(right_sub_array)
+
+    return merge(left_sub_array, right_sub_array)
+
+
+def merge(left_sub_array, right_sub_array):
+    """Merges the sub-arrays together from smallest to biggest element"""
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    # go through both sub-array from left to right appending always the smallest element to the merged array
+    while left_index < len(left_sub_array) and right_index < len(right_sub_array):
+        if left_sub_array[left_index] > right_sub_array[right_index]:
+            merged.append(right_sub_array[right_index])
+            right_index += 1
+        else:
+            merged.append(left_sub_array[left_index])
+            left_index += 1
+
+    # add what is left in one of the arrays
+    merged += left_sub_array[left_index:]
+    merged += right_sub_array[right_index:]
+
+    return merged
+
+"""
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
     solution = test_case[1]
@@ -26,6 +69,10 @@ def test_function(test_case):
         print("Pass")
     else:
         print("Fail")
+"""
 
-test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+
+rearrange_digits([4, 6, 2, 5, 9, 8, 7, 3, 0, 1])
+
+# test_function([[1, 2, 3, 4, 5], [542, 31]])
+# test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
