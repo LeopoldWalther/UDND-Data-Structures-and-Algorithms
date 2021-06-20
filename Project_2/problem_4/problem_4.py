c2a1502f695a638eada63fc647812b7d769011ba
LeopoldWalther <leopold.walther@gmail.com>
2021-06-20 08:19:02 +0200

    feat: solution for problem_4 implemented

---
+++ Project_2/problem_4/problem_4.py
@@ -0,0 +1,66 @@
+# Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+# You're not allowed to use any sorting function that Python provides.
+# Note: O(n) does not necessarily mean single-traversal.
+# If you traverse the array twice, that would still be an O(n) solution but it will not count as single traversal.
+
+
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    if input_list is None:
+        return -1
+
+    zero = []
+    one = []
+    two = []
+
+    for element in input_list:
+        if element == 0:
+            zero.append(element)
+        elif element == 1:
+            one.append(element)
+        elif element == 2:
+            two.append(element)
+        else:
+            return -1
+
+    zero += one + two
+
+    return zero
+
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+print('--------------------given test functions--------------------')
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
+print('--------------------only one kind of digit--------------------')
+test_function([0, 0, 0, 0, 0, 0])
+test_function([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+test_function([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+
+print('--------------------few elements--------------------')
+test_function([1, 0])
+test_function([1])
+
+print('--------------------not allowed digit--------------------')
+test_function([1, 0, 3, 5, 7])
+test_function([8])
+
+print('--------------------empty list--------------------')
+test_function([])
+
+
